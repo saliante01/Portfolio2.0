25e08700a8eb140c6b795d78b37c2f03451ba366
Seba <s.aliante01@ufromail.cl>
2025-03-02 01:35:51 -0300

    feat(images - skills - containers)

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
+  images = ['java3.png.webp','SpringLogo.png', 'klipartz.com-2.png', 'UnityLogo.png','UnityLogo.png','UnityLogo.png'];
 
 }
